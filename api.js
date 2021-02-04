9478b03d4447898a73e9b51b147f9bf99e146b74
Moniur121 <moniurrahman121@gmail.com>
2021-02-04 13:46:27 +0600

    text

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 var inputValue=document.getElementById('inputValue');
 var submitBtn=document.getElementById('submitBtn');
+var weatherIcon=document.getElementById('weatherIcon');
 var cityName=document.getElementById('cityName');
 var temp=document.getElementById('temp');
 var lead=document.getElementById('lead');
@@ -7,12 +8,17 @@ submitBtn.addEventListener('click',function(){
 fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=42192424ff0d8df927979f1b5b69bbd5')
 .then(res=> res.json())
 .then(data=>  {
+    var kelvin=273;
+    var weatherIconValue=data['weather'][0]['icon'];
     var nameValue=data['name'];
-    var tempValue=data['main']['temp'];
+    // var tempValue=data['main']['temp'];
+    var tempValue=Math.floor(data.main.temp-kelvin);
     var leadValue=data['weather'][0]['description'];
+    weatherIcon.innerHTML=weatherIconValue
     cityName.innerHTML=nameValue;
     temp.innerHTML=tempValue;
     lead.innerHTML=leadValue;
+    // console.log(data);
 })
 .catch(err=> alert('wrong city name'))
 })
